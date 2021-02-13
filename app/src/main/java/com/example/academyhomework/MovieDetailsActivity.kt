76b1618c608e2865e5c289be6cf45baf929f0c24
Admin <itachi_172555@mail.ru>
2021-02-13 17:22:48 +0300

    hw init

--- app/src/main/java/com/example/academyhomework/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/academyhomework/MovieDetailsActivity.kt
@@ -3,15 +3,9 @@ package com.example.academyhomework
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_movie_details)
     }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-    }
-
 }
