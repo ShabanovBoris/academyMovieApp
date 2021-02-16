1c6b19e65f822964d8e33e7add5e7b95c9e7dc6d
Admin <itachi_172555@mail.ru>
2021-02-16 12:15:27 +0300

    homework 4 has done

--- app/src/main/java/com/example/academyhomework/MainActivity.kt
+++ app/src/main/java/com/example/academyhomework/MainActivity.kt
@@ -3,18 +3,34 @@ package com.example.academyhomework
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.academyhomework.interfaces.ScreenChangeable
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),ScreenChangeable {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     /** Mockup*/   startActivity(Intent(this, MovieDetailsActivity::class.java))
+     /** Mockup*/   if(savedInstanceState == null)
+        {
+         supportFragmentManager.beginTransaction()
+             .addToBackStack(null)
+             .replace(R.id.containerMainActivity,FragmentMovieList.newInstance("",""))
+             .commit()
+        }
 
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
+    }
+
+    override fun moveTo(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            addToBackStack(null)
+            replace(R.id.containerMainActivity,fragment)
+            commit()
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/academyhomework/interfaces/ScreenChangeable.kt
@@ -0,0 +1,7 @@
+package com.example.academyhomework.interfaces
+
+import androidx.fragment.app.Fragment
+
+interface ScreenChangeable {
+    fun moveTo(fragment:Fragment)
+}
